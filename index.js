a08bdc299e8bcafe2b47c31a8063f8954cab1618
Gonzalo2101 <97133094+Gonzalo2101@users.noreply.github.com>
2023-01-27 13:01:02 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById("perfil").onclick = function () {
+    console.log ("cambiando la foto de perfil")
+    Foto.src = "imagenes/torettoformal.jpg"
+} 
+document.getElementById("default").onclick = function () {
+    console.log ("cambiando la foto de perfil")
+    Foto.src = "imagenes/toretto 4x4.jpg"
+} 
